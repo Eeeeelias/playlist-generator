2715da41152972ab90b5fe837e34e61fcf9bf49a
Eeeeelias <80590426+Eeeeelias@users.noreply.github.com>
2024-08-25 23:34:45 +0200

    check if music library exists

--- PlaylistGen.cs
+++ PlaylistGen.cs
@@ -7,6 +7,8 @@ using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 using Microsoft.Extensions.Logging;
+using MediaBrowser.Model.Tasks;
+using MediaBrowser.Controller.Library;
 
 namespace PlaylistGenerator;
 
@@ -36,6 +38,59 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
             {
                 Name = this.Name,
                 EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+            }
+        };
+    }
+}
+
+
+public class LibraryListingTask : IScheduledTask
+{
+    private readonly ILibraryManager _libraryManager;
+    private readonly ILogger<LibraryListingTask> _logger;
+    private readonly PluginConfiguration _config;
+
+    public LibraryListingTask(ILibraryManager libraryManager, ILogger<LibraryListingTask> logManager)
+    {
+        _libraryManager = libraryManager;
+        _logger = logManager;
+        _config = Plugin.Instance!.Configuration;
+    }
+
+    public string Name => "List Libraries and Config";
+    public string Key => "LibraryListingTask";
+    public string Description => "Lists all libraries and current configuration values.";
+    public string Category => "Library";
+
+    public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+    {
+     
+        _logger.LogInformation("LibraryListingTask started.");
+
+        var libraries = _libraryManager.GetVirtualFolders();
+        bool hasMusicLibrary = libraries.Any(library => library.CollectionType == MediaBrowser.Model.Entities.CollectionTypeOptions.music);
+
+        if (hasMusicLibrary != true)
+        {
+            _logger.LogWarning("No music library found. Aborting");
+            return Task.CompletedTask;
+        }
+
+        _logger.LogInformation("Now doing cool stuff");
+
+        _logger.LogInformation("LibraryListingTask finished.");
+        return Task.CompletedTask;
+    }
+
+    public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+    {
+        return new[]
+        {
+            // Example trigger: Run every day at midnight
+            new TaskTriggerInfo
+            {
+                Type = TaskTriggerInfo.TriggerDaily,
+                TimeOfDayTicks = TimeSpan.FromHours(0).Ticks
             }
         };
     }
