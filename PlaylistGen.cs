ed8666d17b4e574941c087181458da86231ae010
Eeeeelias <80590426+Eeeeelias@users.noreply.github.com>
2024-08-26 20:47:19 +0200

    create & delete playlists

--- PlaylistGen.cs
+++ PlaylistGen.cs
@@ -12,11 +12,13 @@ using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Net;
+using MediaBrowser.Model.Playlists;
 
 using Jellyfin.Data.Entities;
 
 using Jellyfin.Data.Enums;
-using Microsoft.Extensions.Configuration;
+using MediaBrowser.Controller.Playlists;
+using MediaBrowser.Controller.Providers;
 
 namespace PlaylistGenerator;
 
@@ -52,13 +54,18 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 }
 
 
-public class PlaylistGenerationTask(ILibraryManager libraryManager, IUserManager userManager, IUserDataManager userDataManager, ILogger<PlaylistGenerationTask> logManager) : IScheduledTask
+public class PlaylistGenerationTask(ILibraryManager libraryManager, 
+                                    IUserManager userManager, 
+                                    IUserDataManager userDataManager, 
+                                    ILogger<PlaylistGenerationTask> logManager,
+                                    IPlaylistManager playlistManager) : IScheduledTask
 {
     private readonly ILibraryManager _libraryManager = libraryManager;
     private readonly ILogger<PlaylistGenerationTask> _logger = logManager;
     private readonly PluginConfiguration _config = Plugin.Instance!.Configuration;
     private readonly IUserManager _userManager = userManager;
     private readonly IUserDataManager _userDataManager = userDataManager;
+    private readonly IPlaylistManager _playlistManager = playlistManager;
 
 
     public string Name => "Generate personal playlist";
@@ -73,12 +80,9 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager, IUserManager
         _logger.LogInformation("Start generating playlist");
 
         var songList = new List<ScoredSong>();
-        var query = new InternalItemsQuery
-        {
-            IncludeItemTypes = [BaseItemKind.Audio]
-        };
+        var SongQuery = new InternalItemsQuery{IncludeItemTypes = [BaseItemKind.Audio]};
         
-        var songs = _libraryManager.GetItemList(query);
+        var songs = _libraryManager.GetItemList(SongQuery);
 
         if (songs.Count <= 0)
         {
@@ -88,7 +92,7 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager, IUserManager
 
         _logger.LogInformation($"Found {songs.Count} songs");
         
-        var currentUser = _userManager.GetUserByName(_config.PlaylistUserName);
+        User? currentUser = _userManager.GetUserByName(_config.PlaylistUserName);
 
         if (currentUser == null)
         {
@@ -108,6 +112,19 @@ public class PlaylistGenerationTask(ILibraryManager libraryManager, IUserManager
         {
             _logger.LogInformation($"{scoredSong.Song.Name} - Score: {scoredSong.Score}");
         }
+
+        // check if playlist exists
+        PlaylistService playlistServer = new(_playlistManager, _libraryManager);
+        var allPlaylists = _libraryManager.GetItemList(new InternalItemsQuery{IncludeItemTypes = [BaseItemKind.Playlist]});
+
+        if (allPlaylists.Any(playlist => playlist.Name.Equals(_config.PlaylistName))) 
+        {
+            _logger.LogInformation($"Playlist {_config.PlaylistName} exists. Overwriting.");
+            playlistServer.RemovePlaylist(_config.PlaylistName);
+        }
+
+        // make the playlist
+        playlistServer.CreatePlaylist(_config.PlaylistName, currentUser, sortedSongs[0..20]);
 
         _logger.LogInformation("Generated playlist.");
         return Task.CompletedTask;
@@ -134,7 +151,6 @@ public class ScoredSong : BaseItem
     public BaseItem Song { get; set; }
     public User User { get; set; }
     public double Score { get; set; }
-
     
 
     public ScoredSong(BaseItem song, User user, IUserDataManager userDataManager)
@@ -147,6 +163,71 @@ public class ScoredSong : BaseItem
 
     private double CalculateScore()
     {
-        return new Random().NextDouble();
+        var userData = _userDataManager.GetUserData(User.Id, Song);
+        Score = new Random().NextDouble();
+        if (userData.IsFavorite)
+        {
+            Console.WriteLine($"Likes: {userData.Likes}\nPlayCount: {userData.PlayCount
+            }\nLast Played:{userData.LastPlayedDate}\nFavorite:{userData.IsFavorite}");
+            Score++;
+        }
+        return Score;
+    }
+}
+
+
+public class PlaylistService
+{
+    private readonly IPlaylistManager _playlistManager;
+    private readonly ILibraryManager _libraryManager;
+
+    public PlaylistService(IPlaylistManager playlistManager, ILibraryManager libraryManager)
+    {
+        _playlistManager = playlistManager;
+        _libraryManager = libraryManager;
+    }
+
+    public void CreatePlaylist(string playlistName, User user, List<ScoredSong> items)
+    {
+
+        // Create the playlist
+        var request = new PlaylistCreationRequest
+        {
+            Name = playlistName,
+            ItemIdList = items.Select(item => item.Song.Id).ToArray(),
+            MediaType = MediaType.Audio,
+            UserId = user.Id
+        };
+        try 
+        {
+            Console.WriteLine(request.ItemIdList[0]);
+            Console.WriteLine(request.ItemIdList[1]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine($"Playlist not long enough. Songs: {items.Count}");
+        }
+
+
+        var playlist = _playlistManager.CreatePlaylist(request);
+    }
+
+    public void RemovePlaylist(string playlistName)
+    {
+        // Find the playlist by name
+        var playlists = _libraryManager.GetItemList(new InternalItemsQuery
+        {
+            IncludeItemTypes = [BaseItemKind.Playlist]
+        });
+
+        var playlist = playlists.FirstOrDefault(p => p.Name.Equals(playlistName));
+
+        if (playlist != null)
+        {
+            // Delete the playlist
+            var options = new DeleteOptions();
+            _libraryManager.DeleteItem(playlist, options);
+        }
     }
 }
